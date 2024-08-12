bc641253fab4e2568a632be913c82dacd2cd00ae
vldmirr <tsutsura18@gmail.com>
2024-08-12 21:48:14 +0300

    change urls

--- kittenclubinc/blog/models.py
+++ kittenclubinc/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -14,7 +15,7 @@ class Post(models.Model):
 
     #fields
     title=models.CharField(max_length=250)
-    slug=models.SlugField(max_length=250)
+    slug=models.SlugField(max_length=250, unique_for_date='publish')
     
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')#foreign key
 
@@ -37,4 +38,10 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
-    
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
