66b6feb29995e754e3c162a6c147b5b9443c9c88
vldmirr <tsutsura18@gmail.com>
2024-08-13 21:19:07 +0300

    added page 3 to one page split

--- kittenclubinc/blog/views.py
+++ kittenclubinc/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Разбивка 3 постами в одну страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts':posts})
